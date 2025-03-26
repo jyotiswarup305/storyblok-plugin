0010fcc40fc87c2b2df35afbf6080348b4aa4002
jyotiswarup305 <jyotiswarup.jena@cognizant.com>
2025-03-26 20:34:56 +0530

    Updated

--- storyblok-plugin/src/StoryblokPlugin.js
+++ storyblok-plugin/src/StoryblokPlugin.js
@@ -1,15 +1,15 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
- 
+
 const PUBLIC_TOKEN = 'Rczqct1Nf3RlljXADZu85Qtt';
 const MANAGEMENT_API_TOKEN = 'gk09GGED7Vzy4r7I8rTqVQtt-256793-WWo1zBy7CnUDDqxFEBmQ';
- 
+
 const fetchStoryContent = async (storyId) => {
   const url = `https://api-us.storyblok.com/v2/cdn/stories/${storyId}?token=${PUBLIC_TOKEN}`;
   const response = await axios.get(url);
   return response.data.story.content;
 };
- 
+
 const fetchComponentSchema = async (spaceId) => {
   const url = `https://api-us.storyblok.com/v1/spaces/${spaceId}/components`;
   const response = await axios.get(url, {
@@ -20,56 +20,60 @@ const fetchComponentSchema = async (spaceId) => {
   });
   return response.data.components;
 };
- 
-const findOutOfSchemaAttributes = (content, components) => {
-  const outOfSchema = [];
- 
-  const checkAttributes = (content, componentName) => {
+
+const findOutOfSchemaAttributes = (content, components, path = '') => {
+  let outOfSchema = [];
+
+  const checkAttributes = (content, componentName, currentPath) => {
     const component = components.find(c => c.name === componentName);
     if (!component) return;
- 
+
     const schemaAttributes = Object.keys(component.schema);
     const contentAttributes = Object.keys(content);
- 
+
     contentAttributes.forEach(attr => {
       if (attr !== '_uid' && attr !== 'component' && !schemaAttributes.includes(attr)) {
         outOfSchema.push({
           component: componentName,
           attribute: attr,
-          type: typeof content[attr]
+          type: typeof content[attr],
+          path: `${currentPath}.${attr}`
         });
       }
     });
- 
-    if (content.body && Array.isArray(content.body)) {
-      content.body.forEach(block => {
-        if (block.component) {
-          checkAttributes(block, block.component);
-        }
-      });
-    }
+
+    Object.keys(content).forEach(key => {
+      if (Array.isArray(content[key])) {
+        content[key].forEach((block, index) => {
+          if (block.component) {
+            checkAttributes(block, block.component, `${currentPath}.${key}[${index}]`);
+          }
+        });
+      } else if (typeof content[key] === 'object' && content[key] !== null && content[key].component) {
+        checkAttributes(content[key], content[key].component, `${currentPath}.${key}`);
+      }
+    });
   };
- 
-  checkAttributes(content, content.component);
+
+  checkAttributes(content, content.component, path);
   return outOfSchema;
 };
- 
+
 const StoryblokPlugin = ({ storyId, spaceId }) => {
   const [outOfSchemaAttributes, setOutOfSchemaAttributes] = useState([]);
- 
+
   useEffect(() => {
     const fetchData = async () => {
       const content = await fetchStoryContent(storyId);
-      console.log(content);
       const components = await fetchComponentSchema(spaceId);
- 
-      const outOfSchema = findOutOfSchemaAttributes(content, components);
+      
+      const outOfSchema = findOutOfSchemaAttributes(content, components, 'root');
       setOutOfSchemaAttributes(outOfSchema);
     };
- 
+
     fetchData().catch(console.error);
   }, [storyId, spaceId]);
- 
+
   return (
     <div>
       <h1>Out-of-Schema Attributes</h1>
@@ -77,5 +81,5 @@ const StoryblokPlugin = ({ storyId, spaceId }) => {
     </div>
   );
 };
- 
+
 export default StoryblokPlugin;
